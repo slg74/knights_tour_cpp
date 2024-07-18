2d5a8b897d2fbda43889e5338d8723ac24e1129d
slg74 <scottghome@scottgs-MBP.lan>
2024-07-18 15:58:42 -0500

    foolin with cpp

--- play.cpp
+++ play.cpp
@@ -23,5 +23,9 @@ int main(void) {
 
     int num = count_x(a, s);
     std::cout << "found " << s << " " << num << " times in " << a << std::endl;
+
+    // scottghome@scottgs-MBP knights_tour_cpp % g++ -o play play.cpp -std=c++20 && ./play
+    // found a 5 times in abcabcabcabcabc
+
     return 0;
 }
