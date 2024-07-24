acab26a5b3142d2f7f085f0689484faa68ea624a
slg74 <scottghome@scottgs-MBP.lan>
2024-07-24 12:19:29 -0500

    adding 16 and 32 bit samples

--- fft.c
+++ fft.c
@@ -30,23 +30,35 @@ void fft(cplx buf[], int n)
 }
  
  
-void show(const char * s, cplx buf[]) {
+void show(const char * s, cplx buf[], int BITS) {
 	printf("%s", s);
-	for (int i = 0; i < 8; i++)
+	for (int i = 0; i < BITS; i++)
 		if (!cimag(buf[i]))
 			printf("%g ", creal(buf[i]));
 		else
 			printf("(%g, %g) ", creal(buf[i]), cimag(buf[i]));
+    printf("\n"); 
 }
 
 int main()
 {
 	PI = atan2(1, 1) * 4;
-	cplx buf[] = {1, 1, 1, 1, 0, 0, 0, 0};
+	cplx buf_8[] = {1, 1, 1, 1, 0, 0, 0, 0};
+	cplx buf_16[] = {1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0};
+	cplx buf_32[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; 
  
-	show("Data: ", buf);
-	fft(buf, 8);
-	show("\nFFT : ", buf);
- 
+	show("Data (8 bits): ", buf_8, 8);
+	fft(buf_8, 8);
+	show("\nFFT 8: ", buf_8, 8);
+
+	show("Data (16 bits): ", buf_16, 16);
+	fft(buf_16, 16);
+	show("\nFFT 16: ", buf_16, 32);
+
+	show("Data (32 bits): ", buf_32, 32);
+	fft(buf_32, 32);
+	show("\nFFT 32: ", buf_32, 32);
+
 	return 0;
 }
